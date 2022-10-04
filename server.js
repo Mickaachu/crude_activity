1bf68848b0c2a9e0a574ae036774317f69f24094
Erin <valleerin4@gmail.com>
2022-10-04 16:27:23 +0800

    adding delete request

--- server.js
+++ server.js
@@ -14,7 +14,8 @@ app.get('/',(req,res) =>{
 
 /* MEMORY BASED DATABASE */
 const userList = [
-    {id:0, userName:"Claro"}
+    {id:0, userName:"Claro"},
+    {id:1, userName:"Miguel"}
 ]
 
 /* INPUT VALIDATOR */
@@ -27,12 +28,12 @@ function validate(apiRequest) {
 }
 
 /* API GET REQUEST */
-app.get('/api/userList',(req,res) => {
+app.get('/api/user',(req,res) => {
     res.send(userList)
 })
 
 /* API POST REQUEST */
-app.post('/api/addUser',(req,res) =>{
+app.post('/api/user',(req,res) =>{
     // Validate the input
     const {error} = validate(req.body)
     // If the requirements did not meet return 400 Bad Request
@@ -49,6 +50,18 @@ app.post('/api/addUser',(req,res) =>{
     // Show list of users
     res.send(userList)
 })
+
+
+// DELETE REQUEST
+app.delete('api/userdelete/:id', (req,res) => {
+    const user = userList.find(c => c.id === parseInt(req.params.id))
+    if (!user) res.status(404).send("The user with the given ID was not found")
+    const index = userList.indexOf(user)
+    userList.splice(index, 1)
+    res.send(userList)
+})
+
+
 
 /* PORT CONFIG */
 const port = process.env.PORT || 1111

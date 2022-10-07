eab687e61ff95fd18dbe1686e397bd0670f872b2
Claro Recto <88716852+claro-recto-brain-404@users.noreply.github.com>
2022-10-07 17:03:59 +0800

    Update server.js

--- server.js
+++ server.js
@@ -116,19 +116,6 @@ app.delete('/api/deleteUser/:id',(req,res)=>{
     })
 })
 
-// API DELETE REQUEST
-app.delete('/api/deleteUser/:id',(req,res) => {
-    const userExist = userList.find(user => user.id === parseInt(req.params.id))
-   
-    if (!userExist) res.status(400).send("User does not exist");
-    
-    // else delete user data
-    const index = userList.indexOf(userExist)
-    userList.splice(index,1)
-    // Show the updated list
-    res.send(userList)
-})
-
 /* PORT CONFIG */
 const port = process.env.PORT || 1111
 app.listen(port, () => console.log(`Listening on port ${port}...`))

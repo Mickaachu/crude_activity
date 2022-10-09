9f8c7ed212347b8787f34d6796034fc668e50bcf
Erin <valleerin4@gmail.com>
2022-10-09 18:32:36 +0800

    fecthing data from backend

--- crud_client/pages/index.js
+++ crud_client/pages/index.js
@@ -1,6 +1,31 @@
-import {userState} from "react";
+import {useState, useEffect} from "react";
 
 const Home = () => {
-  const [user, userEffect] = userState(null);
+  const [users, setUser] = useState(null);
+  useEffect(() => {
+    fetch("http://localhost:3000/api/users")
+      .then((res) => res.json())
+      .then((data) => setUser(data));
+  }, []);
+  return (
+      <div className="container">
+        <h1>Welcome!</h1>
+        <h2>Please see Users below</h2>
+        <ul>
+          {users ? (
+            users.map((user) => (
+              <li>
+                {user.userName}
+              </li>
+            ))
+          ) : (
+            <div>Loading...</div>
+          )}
+          
 
+        
+        
+        </ul>
+      </div>
+    );
 }
